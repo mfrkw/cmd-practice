1d12a6c0ab02cdede3fe1d3e059cec8257a30282
mfrkw <xjdk3766@gmail.com>
2021-10-27 22:02:26 +0900

    [複数の値渡し]

--- Practice.java
+++ Practice.java
@@ -1,10 +1,10 @@
 public class Practice {
 	public static void main (String[] agrs){
 		System.out.println("メソッドを呼び出す");
-		hello("かおり" );
+		hello("かおり", "よしこ" );
 	}
 	
-	public static void hello(String name){   //mainのかおり実引数が仮引数に値渡しされる
-		System.out.println(name + "さんこんにちは");
+	public static void hello(String name1, String name2){   //mainの実引数が仮引数に値渡しされる
+		System.out.println(name1 + "さんこんにちは" + name2 + "さんもいたんですね！");
 	}
-}
+}    //引数を複数にしてもhelloメソッドに順番に値渡しされる
