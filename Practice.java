60dfc8d8d0f647c9a86f250650a8d2ffcc1a822d
mfrkw <xjdk3766@gmail.com>
2021-10-30 11:35:58 +0900

    [オーバーロード]

--- Practice.java
+++ Practice.java
@@ -2,7 +2,7 @@ public class Practice {
 	public static void main (String[] agrs){
 		System.out.println("メソッドを呼び出す");
 		hello("かおり", "よしこ" );
-		item("ピザ", 500);   //商品のメソッド
+		print(item("ポテト","ハンバーガー"), 1000);
 	}
 	
 	public static void hello(String name1, String name2){   //mainの実引数が仮引数に値渡しされる
@@ -10,7 +10,15 @@ public class Practice {
 	}
    //引数を複数にしてもhelloメソッドに順番に値渡しされる
 	
-	public static void item(String itemname, int price){
-		System.out.println(itemname + "でお間違いないでしょうか？" + price + "円でございます。");
+	public static String item(String itemname){     //item("ポテト","ハンバーガー")あくまで商品名だけここまで　　　　　オーバーロード
+		return itemname ;
+	}
+	
+	public static String item(String itemname, String itemname2){
+		return itemname + "と" + itemname2 ;
+	}
+	
+	public static void print(String  itemName, int price){
+		System.out.println(itemName + "でお間違いないでしょうか？" + price + "円でございます。");
 	}
 }
