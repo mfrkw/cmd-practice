de1fe5369d40c63b83fef889a24ddcbb243191ca
mfrkw <xjdk3766@gmail.com>
2021-10-31 21:18:15 +0900

    [割引計算追加]

--- Task1.java
+++ Task1.java
@@ -42,12 +42,21 @@
 	**  - 値引き額は小数点以下切り捨てで計算する（キャストを使いましょう）
 	**  - 割引金額の上限は5000円とする
  	*/
-	static int discount( int price ){            
+	static int discount( int pr ){            
 		//仕様に書かれているマジックナンバー（リテラル）は定数で定義する
 		final int discountBorder1 = 1000;   //値引き発生値段1000円
-		final int discountBorder2 = 5000;   //値引き発生値段5000円
+		final int discountBorder2 = 5000;   //値引き発生値段5000円           ①
 		final double discountRate1 = 0.1;   //1000円以上5000円以下の割引率
 		final double discountRate2 = 0.2;   //5000円以上の割引率
-		final int discountPraiceMax = 5000; //割引金額の上限5000円
+		final int discountPraiceMax = 5000; //割引金額の上限5000円           ②
+																		// ①と②を使い回さない！  上限が変わる可能性もあるので変数をそれぞれ定義する
+		int priceDowntemp = 0;      //仮の値引き額
+		
+		if(discountBorder1<= pr && pr <= discountBorder2){
+			priceDowntemp = (int)((pr - discountBorder1) * discountRate1);   //1000円より大きい分について10％割引にする
+		
+		}else if(pr > discountBorder2){
+			priceDowntemp = (int)(discountBorder2 - discountBorder1) * discountRate1
+		}
 	}
 }
